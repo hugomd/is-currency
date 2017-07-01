d9f2b53bd5cb2ccfcbc51bca3f402f26423db11f
Hugo <h@hugo.sx>
2017-07-01 14:32:11 +1000

    ✨ Validate currency symbols and codes, combined
    Closes #2

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-const currencyCodes = require('./currency-codes');
+const isCurrencyCode = require('is-currency-code');
+const isCurrencySymbol = require('is-currency-symbol');
 
-module.exports = code => {
-  if (typeof code === 'string') {
-    return currencyCodes.indexOf(code.toLowerCase()) !== -1;
+const stripAmount = input => {
+  const decimalPattern = /\d+(\.\d{1,2})?/;
+  const whitespacePattern = / /g;
+  return input
+    .replace(decimalPattern, '')
+    .replace(whitespacePattern, '');
+};
+
+const validateSymbolAndCode = input => {
+  const stripped = stripAmount(input);
+
+  // Last three must be currency code
+  const code = stripped.substr(-3);
+
+  const symbol = stripped.substr(0, stripped.length - 3);
+
+  return isCurrencyCode(code) && isCurrencySymbol(symbol);
+};
+
+module.exports = input => {
+  if (typeof input === 'string') {
+    return (
+      isCurrencyCode(input) ||
+      isCurrencySymbol(input) ||
+      validateSymbolAndCode(input)
+    );
   }
   return false;
 };
--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const test = require('ava');
 const isCurrency = require('./index');
 
 test('should succeed with uppercase currency code', t => {
-  t.is(isCurrency('usd'), true);
+  t.is(isCurrency('USD'), true);
 });
 
 test('should succeed with lowercase currency code', t => {
@@ -12,6 +12,38 @@ test('should succeed with lowercase currency code', t => {
 
 test('should fail with invalid currency code', t => {
   t.is(isCurrency('not-a-currency-code'), false);
+});
+
+test('should succeed with currency symbol', t => {
+  t.is(isCurrency('$'), true);
+});
+
+test('should succeed with currency amount', t => {
+  t.is(isCurrency('$5.00 AUD'), true);
+});
+
+test('should succeed with valid lowercase currency code in currency amount', t => {
+  t.is(isCurrency('$5.00aud'), true);
+});
+
+test('should succeed with valid lowercase currency code in currency amount with space', t => {
+  t.is(isCurrency('$5.00 aud'), true);
+});
+
+test('should succeed with currency amount with space', t => {
+  t.is(isCurrency('$5.00 AUD'), true);
+});
+
+test('should fail with invalid currency symbol in currency amount', t => {
+  t.is(isCurrency('#5.00AUD'), false);
+});
+
+test('should fail with invalid currency code in currency amount', t => {
+  t.is(isCurrency('$5.00XXX'), false);
+});
+
+test('should fail with invalid currency code and symbol in currency amount', t => {
+  t.is(isCurrency('#5.00XXX'), false);
 });
 
 test('should fail with boolean', t => {
